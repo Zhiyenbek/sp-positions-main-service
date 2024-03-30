20d832b2db5dbf24416186f7cdf2e98ac003788b
zk <thisnotbad@gmail.com>
2024-03-30 17:07:08 +0500

    remove omitempty

--- internal/models/positions.go
+++ internal/models/positions.go
@@ -7,14 +7,14 @@ type Position struct {
 	Skills            []*string `json:"skills"`
 	Company           *Company  `json:"company,omitempty"`
 	RecruiterPublicID *string   `json:"recruiter_public_id,omitempty"`
-	Description       *string   `json:"description,omitempty"`
+	Description       *string   `json:"description"`
 }
 
 type Company struct {
 	PublicID    *string `json:"public_id"`
 	Name        *string `json:"name"`
-	Logo        *string `json:"logo,omitempty"`
-	Description *string `json:"description,omitempty"`
+	Logo        *string `json:"logo"`
+	Description *string `json:"description"`
 }
 
 type Question struct {
